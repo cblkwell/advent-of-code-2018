3fbc60df84f70d7780ed37356ae4141b585b08b1
cblkwell <blackisis@gmail.com>
2018-12-16 12:11:01 -0800

    day 5 part 2 complete

--- day5/day5.py
+++ day5/day5.py
@@ -1,10 +1,11 @@
 from sys import argv
 import re
+import string
 
 inputfile = argv[1]
 
-with open(inputfile) as file:
-    polymer = list(file.read().rstrip())
+def react_polymer(input_poly):
+    polymer = list(input_poly)
     index = 0
     reacted_polymer = []
     same_unit = re.compile(r'(\w)\1', re.IGNORECASE)
@@ -21,6 +22,28 @@ with open(inputfile) as file:
             else:
                 reacted_polymer.append(polymer[index])
                 index += 1
+    
+    return reacted_polymer
+
+
+with open(inputfile) as file:
+    polymer = file.read().rstrip()
+    reacted_polymer = react_polymer(polymer)
+    shortest_polymer = tuple()
+    for element in string.ascii_lowercase:
+        cap_element = element.capitalize()
+        new_polymer = polymer.replace(element, "").replace(cap_element, "")
+        print(new_polymer)
+        new_polymer = react_polymer(new_polymer)
+        if len(shortest_polymer) == 0:
+            shortest_polymer = (new_polymer, len(new_polymer))
+        else:
+            if len(new_polymer) < shortest_polymer[1]:
+                shortest_polymer = (new_polymer, len(new_polymer)) 
+      
+        
 
 print(f"The reacted polymer is {''.join(reacted_polymer)}")
 print(f"The length of the reacted polymer is {len(reacted_polymer)}.")
+print(f"But the shortest improved polymer is {''.join(shortest_polymer[0])}")
+print(f"It is {shortest_polymer[1]} long.")
